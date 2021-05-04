5233b6108ea37625e88cc1ceeb1b252d62dcfa48
theticarcher38 <hunterleegoodman@gmail.com>
2021-05-04 14:55:13 +0000

    Made Changes

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+var randString = Math.random().toString(36).replace(/[^a-zA-Z0-9]+/g, '').substr(0, 300);
+
+console.log(randString);
+
+function stringToHash(string) {
+
+    var hash = 0;
+
+    if (string.length == 0) return hash;
+
+    for (i = 0; i < string.length; i++) {
+        char = string.charCodeAt(i);
+        hash = ((hash << 5) - hash) + char;
+        hash = hash & hash;
+    }
+    return hash;
+}
+
+var hashStr = stringToHash(randString);
+
+console.log(hashStr);
+
